74272e83ca3648e82e6c01b54ccc9aab84310b99
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 22:10:16 +0700

    first commit

---
+++ no3.js
@@ -0,0 +1,6 @@
+var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+const baru = kodeUnix.map(a=>kodeUnix.length-50+kodeUnix.length)
+.filter(a=>a<20)
+.reduce((a<20,kodeUnix.length)(a=>a<20 + kodeUnix.length))
+
+console.log(baru+kodeUnix.length);
